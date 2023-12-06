37f4feb4aef90556846d672f7b58bd1fe017f3cf
alberto9701 <beto970154@gmail.com>
2023-12-06 13:05:28 -0600

    agregando código php

---
+++ pages/UnidadApren.php
@@ -0,0 +1,16 @@
+<?php
+    class UnidadAprend{
+        public $semestre;
+        public $nombre;
+        public $estado;
+
+        public function __construct($semestre, $nombre, $estado){
+            $this->semestre=$semestre;
+            $this->$nombre = $nombre;
+            $this->estado = $estado;
+        }
+    }
+
+    //obtenemos los archivos 
+    
+?>
---
+++ pages/configura.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+
+    <?php
+        $numero1 = 5;
+        $numero2 = 10;
+        echo "este es un numero";
+        echo $numero1+$numero2;
+
+
+        $tablaEnc = 
+            
+    ?>
+
+    <?php
+        echo 'esto es una tabla';
+        echo '<iframe src="./tablaPrimaria.html" frameborder="0"></iframe>';
+        
+
+    ?>
+
+    
+
+</body>
+</html>
