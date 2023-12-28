a2b840e24b3ebbfc63643dc5ae78dc8f533dbe37
alberto9701 <beto970154@gmail.com>
2023-12-27 19:49:17 -0600

    agregando tabla de cualidades

--- pages/UnidadApren.php
+++ pages/UnidadApren.php
@@ -1,51 +1,260 @@
-<?php
-    class UnidadAprend{
-        public $semestre;
-        public $nombre;
-        public $estado;
-
-        public function __construct($semestre, $nombre, $estado){
-            $this->semestre=$semestre;
-            $this->nombre = $nombre;
-            $this->estado = $estado;
-        }
-    }
-
-    //obtenemos los archivos 
-    $archivo = fopen("unidadesAprendizaje.txt","r");
-
-    $materias = [];
-
-    while(!feof($archivo)){
-        $linea = fgets($archivo);
-        $datos = explode("," ,$linea);
-
-        $materias[] = new UnidadAprend($datos[0],$datos[1],$datos[2]);
-    }
-
-    fclose($archivo);
-
-?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../styles/escom.css">
+    <link rel="stylesheet" href="../styles/principalStyles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed:wght@100&display=swap" rel="stylesheet">
 </head>
 <body>
-    <table>
-        <tr>
-            <?php
+
+    <header class="header">
+        <h1>Mi Recorrido por ESCOM</h1>
+    </header>
+    
+    <nav class="barraDeNav">
+        <a href="../index.html">Inicio</a>
+        <a href="../pages/escuelas.html">Mi vida académica</a>
+        <a href="../pages/UnidadApren.php">Mi recorrido por ESCOM</a>
+        <a href="../pages/cualidades.php">Mis cualidades y habilidades</a>
+        
+    </nav>
+    
+    <div class="contenedor">
+
+        
+        <?php
+            class UnidadAprend{
+                public $semestre;
+                public $nombre;
+                public $estado;
+
+                public function __construct($semestre, $nombre, $estado){
+                    $this->semestre=$semestre;
+                    $this->nombre = $nombre;
+                    $this->estado = $estado;
+                }
+            }
+            
+            //obtenemos los archivos 
+            $archivo = fopen("../datosUsuario/unidadesAprendizaje.txt","r");
+
+            $materias = [];
+
+            while(!feof($archivo)){
+                $linea = fgets($archivo);
+                $datos = explode("," ,$linea);
+
+                $materias[] = new UnidadAprend($datos[0],$datos[1],trim($datos[2]));
+            }
+            
+            fclose($archivo);
+            
+            //desplegamos deacuerdo al semestre
+            echo "<table>";
+
+                echo "<div class = 'semestre'>";
+                echo "<h2>Semestre 8</h2>";
                 foreach($materias as $materia){
-                    foreach($materia as $atributo){
-                        echo "<th> <?php $atributo ?></th>";
+                    if( $materia->semestre == 8){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>".$materia->estado."</p>";
+                        echo "</div>";
                     }
                 }
+                echo "</div>";
+                
+                echo "<hr class='division'>";
+
+                echo "<div class = 'semestre'>";
+                echo "<h2>Semestre 7</h2>";
+                foreach($materias as $materia){
+                    if( $materia->semestre == 7){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>".$materia->estado."</p>";
+                        echo "</div>";
+                    }
+                }
+                echo "</div>";
+                
+                echo "<hr class='division'>";
+                
+
+                echo "<div class = 'semestre'>";
+                echo "<h2>Semestre 6</h2>";
+                foreach($materias as $materia){
+                    if( $materia->semestre == 6){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>".$materia->estado."</p>";
+                        echo "</div>";
+                    }
+                }
+                echo "</div>";
+                
+                echo "<hr class='division'>";
+                echo "<div class = 'semestre'>";
+                echo "<h2>Semestre 5</h2>";
+                foreach($materias as $materia){
+                    if( $materia->semestre == 5){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>".$materia->estado."</p>";
+                        echo "</div>";
+                    }
+                }
+                echo "</div>";
+                
+                echo "<hr class='division'>";
+                
+                echo "<div class = 'semestre'>";
+                echo "<h2>Semestre 4</h2>";
+                foreach($materias as $materia){
+                    if( $materia->semestre == 4){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>".$materia->estado."</p>";
+                        echo "</div>";
+                    }
+                }
+                echo "</div>";
+                
+                echo "<hr class='division'>";
+                
+                echo "<div class = 'semestre'>";
+                echo "<h2>Semestre 3</h2>";
+                foreach($materias as $materia){
+                    if( $materia->semestre == 3){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>".$materia->estado."</p>";
+                        echo "</div>";
+                    }
+                }
+                echo "</div>";
+                
+                echo "<hr class='division'>";
+                
+                echo "<div class = 'semestre'>";
+                echo "<h2>Semestre 2</h2>";
+                foreach($materias as $materia){
+                    if( $materia->semestre == 2){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+        
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>".$materia->estado."</p>";
+                        echo "</div>";
+                    }
+                }
+                echo "</div>";
+                
+                echo "<hr class='division'>";
+                
+                
+                echo "<div class='semestre'>";
+                echo "<h2>Semestre 1</h2>";
+                foreach($materias as $materia){
+                    if( $materia->semestre == 1){
+                        if($materia->estado=="aprobada"){
+                            echo "<div class='materia aprobada'>";
+                        }
+                        else if($materia->estado ==="reprobada"){
+                            echo "<div class='materia reprobada'>";
+                        }
+                        else if($materia->estado=="no cursada"){
+                            echo "<div class='materia noCursada'>";
+        
+                        }
+                        echo "<p>". "Semestre: ". $materia->semestre."</p>";
+                        echo "<p>". $materia->nombre."</p>";
+                        echo "<p>". $materia->estado."</p>";
+                        echo "</div>";
+                        }
+                    }
+                echo "</div>";
+            echo "</table>";
+            
+            
             ?>
-        </tr>
-    </table>
+    </div>
 </body>
 </html>
+
+
 
---
+++ pages/cualidades.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/cualidades.css">
+    <link rel="stylesheet" href="../styles/principalStyles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed:wght@100&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <h1>Mi Yo del 2028</h1>
+    </header>
+
+
+    <nav class="barraDeNav">
+        <a href="../index.html">Inicio</a>
+        <a href="../pages/escuelas.html">Mi vida académica</a>
+        <a href="../pages/UnidadApren.php">Mi recorrido por ESCOM</a>
+        <a href="../pages/cualidades.php">Mis cualidades y habilidades</a>
+    </nav>
+
+    <div class="contenedor">
+        <?php
+            class Cualidad{
+                Public $cualidad;
+                Public $calif;
+
+                public function __construct($cualidad,$calif){
+                    $this->cualidad = $cualidad;
+                    $this->calif = $calif;
+                }
+            }
+
+
+            //obtenemos un arreglo de objetos de la clase cualidad que provienen del archivo cualidad.txt
+            $archivo = fopen("../datosUsuario/cualidades.txt","r");
+            $cualidades = array();
+            while(!feof($archivo)){
+                $linea = fgets($archivo);
+                $datos = explode("," ,$linea);
+
+
+                $cualidad = new Cualidad($datos[0],$datos[1]);
+                array_push($cualidades,$cualidad);
+            }
+            fclose($archivo);
+
+
+            //creamos la tabla con las cualidades
+            echo "<table class='tabla'>";
+                echo "<thead class='cabezeraTabla'>";
+                    echo "<td align='cen'>Cualidad</td>";
+                    echo "<td>Calificación</td>";
+                echo "</thead>";
+
+                echo "<tbody class='cuerpoTabla'>";
+                    foreach ($cualidades as $cualidad) {
+                        echo "<tr>";
+                        echo "<td align='center'>".$cualidad->cualidad."</td>";
+
+                        $i = 0;
+                        echo "<td align='center'>";
+                        while($i < $cualidad->calif){
+                            echo "<img class='estrella' src='../images/estrella2.png' alt='estrella'>";
+                            $i++;
+                        }
+                        echo "</td>";
+
+                        //echo "<td>".$cualidad->calif."</td>";
+                        echo "</tr>";
+                    }
+                echo "</tbody>";
+            echo "</table>";
+
+
+
+        ?>
+    </div>
+
+</body>
+</html>
